e75001ccfed869ee3f6a4a2faca0d3a115f9cc2a
vancamargo <vanessacamargo.v@gmail.com>
2018-09-28 18:08:47 -0300

    base de dados

--- src/main/java/com/camargo/resourses/UserResource.java
+++ src/main/java/com/camargo/resourses/UserResource.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.camargo.domaim.User;
 import com.camargo.services.UserService;
+
 
 @RestController
 @RequestMapping(value="/users")
--- src/main/java/com/camargo/services/UserService.java
+++ src/main/java/com/camargo/services/UserService.java
@@ -6,17 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.camargo.domaim.User;
-import com.camargo.repository.userRepository;
+import com.camargo.repository.UserRepository;
+
 
 @Service
 
 public class UserService {
 	
 	@Autowired
-	private userRepository repo;
+	private UserRepository repo;
 	
-	public List<User> findAll(){
-		return repo.findAll();
-	}
+public List<User> findAll(){
+	return repo.findAll();
+}
 
 }
