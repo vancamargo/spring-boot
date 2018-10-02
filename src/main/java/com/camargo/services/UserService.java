789d8dd8da191dbdb85a4247bdd8959f2f7e5073
vancamargo <vanessacamargo.v@gmail.com>
2018-10-02 18:00:59 -0300

    usuario por id

--- src/main/java/com/camargo/services/UserService.java
+++ src/main/java/com/camargo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.camargo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.camargo.domaim.User;
 import com.camargo.repository.UserRepository;
+import com.camargo.services.exception.ObjectNotFoundException;
 
 
 @Service
@@ -19,5 +21,12 @@ public class UserService {
 public List<User> findAll(){
 	return repo.findAll();
 }
+
+public User findById(String id) {  
+	Optional<User> obj = repo.findById(id);  
+	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	} 
+
+
 
 }
