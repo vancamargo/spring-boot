b7801223bafad639630b2e9c4bc446506ddd1740
vancamargo <vanessacamargo.v@gmail.com>
2018-10-03 16:46:12 -0300

    create

--- src/main/java/com/camargo/resourses/UserResource.java
+++ src/main/java/com/camargo/resourses/UserResource.java
@@ -1,14 +1,17 @@
 package com.camargo.resourses;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.camargo.domaim.User;
 import com.camargo.dto.UserDTO;
@@ -30,11 +33,20 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+
 	
 
 }
--- src/main/java/com/camargo/services/UserService.java
+++ src/main/java/com/camargo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.camargo.domaim.User;
+import com.camargo.dto.UserDTO;
 import com.camargo.repository.UserRepository;
 import com.camargo.services.exception.ObjectNotFoundException;
 
@@ -26,6 +27,14 @@ public User findById(String id) {
 	Optional<User> obj = repo.findById(id);  
 	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	} 
+
+public User insert(User obj) {
+	return repo.insert(obj);
+}
+
+public User fromDTO(UserDTO objDto) {
+	return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+}
 
 
 
